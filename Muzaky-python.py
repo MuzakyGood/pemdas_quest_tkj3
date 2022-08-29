02253d9171df51261530269cf1543d5c38aa6a54
MuzakyGood <112367361+MuzakyGood@users.noreply.github.com>
2022-08-29 12:54:21 +0700

    Add files via upload

---
+++ Muzaky-python.py
@@ -0,0 +1,14 @@
+'''Buatlah 10 baris pesan programan perkenalan variabel'''
+
+nama = input("Siapakah Nama Kamu?")
+umur = input("Berapa Umur Kamu") 
+sekolah = input("asal sekolah Kamu?")
+kelas = input("Kelas berapa Anda?")
+jurusan = input("jurusan apa?(jawab jika ada)")
+tujuanhidup = input("Sebutkan tujuan hidup anda?")
+hobi = input("apa hobi anda?")
+makanan = input("makanan favorit?")
+game = input("video game kesukaan?")
+orangtua = input("nama orang tua?")
+
+print("nama saya adalah",nama,"umur saya",umur,"Saya berasal dari sekolah",sekolah,"saya kelas",kelas,"Saya Berasal Dari Jursan",jurusan,"Tujuan hidup saya",tujuanhidup,"hobi saya",hobi,"makanan kesukaan saya adalah",makanan,"Video Game Kesukaan saya",game,"nama orang tua saya",orangtua," Dah itu aja Terima kasih")
