38c6e3a22b9e960ab0ffbba3e6b6b0741e333e15
Muhammad Muzaky Ulum <112367361+MuzakyGood@users.noreply.github.com>
2022-09-01 20:15:25 -0700

    Add files via upload

---
+++ Python5-tkj3.py
@@ -0,0 +1,51 @@
+'''Bagun datar
+   - Persegi
+   - Segitiga
+   - Jajargenjang
+   - Lingkaran'''
+
+# Rumus Persegi Sisi * Sisi
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("Luas Persegi Adalah", luas_p)
+
+# Rumus segitiga A * T / 2
+
+alas = 20
+tinggi = 10
+luas_s = alas * tinggi / 2
+
+print("Luas Segitiga adalah", luas_s)
+
+# Rumus jajargenjang alas * tinggi
+
+alas = 8
+tinggi = 4
+luas_j = alas * tinggi
+
+print("luas jajargenjang adalah", luas_j)
+
+# Rumus Lingkaran phi * r * r
+
+phi = 22/7
+r = 7
+r = 7
+
+luas_1 = phi * r * r
+
+print("luas lingkaran adalah", luas_1)
+
+
+# Program Dinamis
+
+print("\n1.Luas Persegi")
+sisi = int(input("Masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("Luas persegi adalah", luas_p)
+
+
+
+
