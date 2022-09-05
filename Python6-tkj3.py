def8f69d16921c640b759997c112bc46ce32ba14
Muhammad Muzaky Ulum <112367361+MuzakyGood@users.noreply.github.com>
2022-09-04 22:17:12 -0700

    Add files via upload

---
+++ Python6-tkj3.py
@@ -0,0 +1,21 @@
+#Program Konvesi mata uang
+
+#dolar ke Rupiah
+print("\n1.Dolar ke Won")
+dolar_amerika = int(input("masukan nilai dolar amerika Anda:"))
+rupiah17 = dolar_amerika * 14916.50
+print("Nilai rupiah Indonesia adalah",rupiah17)
+
+#Euro ke Won
+print("\n2.Euro ke Won")
+euro_eropa = int(input("Masukan Nilai Euro Eropa Anda:"))
+won_korea = euro_eropa * 1356.10
+print("Nilai Won Korea Selatan Adalah",won_korea)
+
+#Yen Ke Rupiah
+print("\n3.Yen Ke Rupiah")
+yen_jepang = int(input("Masukan Nilai Yen Jepang Anda:"))
+rupiah_indonesia = yen_jepang * 106
+print("Nilai Rupiah Indonesia adalah",rupiah_indonesia)
+
+
