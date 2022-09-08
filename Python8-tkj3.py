bf76586d7e8295a7905363e70f7c3b45ae23d22e
Muhammad Muzaky Ulum <112367361+MuzakyGood@users.noreply.github.com>
2022-09-07 23:37:39 -0700

    Add files via upload

---
+++ Python8-tkj3.py
@@ -0,0 +1,30 @@
+
+list_kosong = []
+
+list_string = ["Muzaky","Dzaky","Arief","Wisang","Haidar"]
+#               0         1      2      3        4
+list_integer = [17,15,8,5,14]
+
+list_mix = ["Muzaky",15,True, 65.0]
+
+print("\nNama-nama Random (list string) Dan Tanggal Lahir (list integer):")
+
+print("\nNama berikut:", list_string[3],"Tanggal lahir:", list_integer[3])
+
+print("Nama berikut:", list_string[0],"Tanggal lahir:", list_integer[0])
+
+print("Nama berikut:", list_string[4],"Tanggal lahir:", list_integer[4])
+
+print("Nama berikut:", list_string[2],"Tanggal lahir:", list_integer[2])
+
+print("Nama berikut:", list_string[1],"Tanggal lahir:", list_integer[1])
+
+
+
+
+
+
+
+
+
+
