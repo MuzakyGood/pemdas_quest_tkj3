c8c0b70c36d55227fe3563c9b10a6b1d07623857
Muhammad Muzaky Ulum <112367361+MuzakyGood@users.noreply.github.com>
2022-09-07 23:37:11 -0700

    Add files via upload

---
+++ Python - Latihan.py
@@ -0,0 +1,54 @@
+'''
+Buatlah Program Sederhana
+5 Bangun datar (bebas)
+script versi kalian
+nama File: Python - Latihan.py
+Kerjakan Sampai 13:15 WIB
+'''
+
+#1.Luas Bangun Datar Persegi
+
+print("\n1.Luas Bangun Datar Persegi")
+sisi1 = int(input("Masukan Input Sisi:"))
+luas_persegi = sisi1 * sisi1
+
+print("Luas Persegi adalah",luas_persegi)
+
+#2.Luas Bangun Datar Segitiga
+
+print("\n2.Luas Bangun Datar Segitiga")
+alas_s = int(input("Masukan Input alas:"))
+tinggi_s = int(input("Masukan Input Tinggi:"))
+luas_s = alas_s * tinggi_s / 2
+
+print("Luas Segitiga Adalah",luas_s)
+
+#3.Luas Bangun Datar Jajar Genjang
+
+print("\n3.Luas Bangun Datar jajar Genjang")
+alas_j = int(input("Masukan Input Alas j:"))
+tinggi_j = int(input("Masukan Input Tinggi j:"))
+luas_j = alas_j * tinggi_j
+
+print("Luas Bangun Datar Jajar Genjang Adalah",luas_j)
+
+#4.Luas Bangun Datar Lingkaran
+
+print("\n4.Luas Bangun Datar Lingkaran")
+phi = 22/7
+rusuk1 = int(input("Masukan Input Rusuk1 Lingkaran:"))
+rusuk2 = int(input("Masukan Input Rusuk2 Lingkaran:"))
+luas_l = phi * rusuk1 * rusuk2
+
+print("Luas lingkaran adalah",luas_l)
+
+#5.Luas Bangun Datar Persegi Panjang
+
+print("\n5.Luas Bangun Datar Persegi Panjang")
+panjang = int(input("Masukan input panjang:"))
+lebar = int(input("Masukan input lebar:"))
+luas_persegipanjang = panjang * lebar
+
+print("Luas Persegi Panjang Adalah",luas_persegipanjang)
+
+               
